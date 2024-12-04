d1904b4e329addf91271bb885e1d2d524b48f867
Rohan <rohan@rootficus.com>
2024-12-04 14:50:02 +0530

    Add Files.

---
+++ bdpass/lib/view/screens/authSection/account_recovery_screen.dart
@@ -0,0 +1,293 @@
+import 'package:BDPass/model/apis/api_response.dart';
+import 'package:BDPass/utils/Util.dart';
+import 'package:BDPass/view/component/custom_button_component.dart';
+import 'package:BDPass/view_model/main_view_model.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import '../../../model/response/setUpAccountResponse.dart';
+import '../../../theme/AppColor.dart';
+import '../../../utils/Helper.dart';
+import '../../component/connectivity_service.dart';
+import '../../component/toastMessage.dart';
+
+class AccountRecoveryScreen extends StatefulWidget {
+  final String? userId; // Define the 'data' parameter here
+
+  AccountRecoveryScreen({Key? key, this.userId}) : super(key: key);
+
+  @override
+  _AccountRecoveryScreenState createState() => _AccountRecoveryScreenState();
+}
+
+class _AccountRecoveryScreenState extends State<AccountRecoveryScreen> {
+  bool passwordVisible = false;
+  bool confirmPasswordVisible = false;
+  bool isLoading = false;
+  final ConnectivityService _connectivityService = ConnectivityService();
+
+  bool inputValid = false;
+  bool isDarkMode = false;
+  late double screenWidth;
+
+  @override
+  void initState() {
+    super.initState();
+    passwordVisible = true;
+    confirmPasswordVisible = true;
+    inputValid = false;
+    isDarkMode = false;
+  }
+
+  void _isValidInput() {
+    //print(input);
+    if (_emailController.text.isNotEmpty &&
+        _nameController.text.isNotEmpty &&
+        _lastNameController.text.isNotEmpty &&
+        _passwordController.text.isNotEmpty &&
+        _dateController.text.isNotEmpty &&
+        _confirmPasswordController.text.isNotEmpty &&
+        _passwordController.text.length >= 8 &&
+        _passwordController.text == _confirmPasswordController.text &&
+        EmailValidator.validate(_emailController.text)) {
+      setState(() {
+        inputValid = true;
+      });
+    } else {
+      setState(() {
+        inputValid = false;
+      });
+    }
+  }
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+  TextEditingController();
+
+  Future<Widget> getSetUpAccountWidget(BuildContext context,
+      ApiResponse apiResponse) async {
+    SetUpAccountResponse? setUpAccountResponse =
+    apiResponse.data as SetUpAccountResponse?;
+    String? message = apiResponse.message.toString();
+    setState(() {
+      isLoading = false;
+    });
+    switch (apiResponse.status) {
+      case Status.LOADING:
+        return Center(child: CircularProgressIndicator());
+      case Status.COMPLETED:
+        print("GetSetUpAccountWidget : ${setUpAccountResponse?.firstName}");
+        await Helper.saveProfileDetails(setUpAccountResponse);
+        if (await Helper.saveProfileDetails(setUpAccountResponse))
+          print("data saved");
+        else
+          print("not saved");
+
+        await Helper.savePassword(_passwordController.text);
+        await Helper.saveCountry(setUpAccountResponse?.countryName);
+        await Helper.saveKycStatus(setUpAccountResponse?.kycStatus);
+        String? password = await Helper.getPassword();
+        print("password: ${password}");
+        await Helper.getUserDetails();
+
+        Navigator.pushReplacementNamed(context, '/BottomNav');
+        return Container(); // Return an empty container as you'll navigate away
+      case Status.ERROR:
+        ToastComponent.showToast(context: context, message: message);
+        return Center(
+          child: Text('Please try again later!!!'),
+        );
+      case Status.INITIAL:
+      default:
+        return Center(
+          child: Text(''),
+        );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    isDarkMode = Theme
+        .of(context)
+        .brightness == Brightness.dark;
+    screenWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+    double screenHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
+    ApiResponse apiResponse = Provider
+        .of<MainViewModel>(context)
+        .response;
+    return Scaffold(
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            hideKeyBoard();
+          },
+          child: Stack(
+            children: [
+              SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: screenHeight * 0.95),
+                  child: Padding(
+                      padding:
+                      const EdgeInsets.only(left: 16.0, right: 16, top: 12),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 12),
+                              GestureDetector(
+                                  onTap: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Icon(Icons.arrow_back)),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              _buildLabelText(context, "Account Recovery", 24,
+                                  true, AppColor.TEXT_COLOR),
+                              SizedBox(height: 4),
+                              _buildLabelText(
+                                  context,
+                                  "To initiate the account recovery process, please enter the email/mobile/emirates ID registered with your BD PASS account.",
+                                  12,
+                                  false,
+                                  AppColor.TEXT_COLOR),
+                              SizedBox(height: 25),
+                              _buildPhoneInput(
+                                  context,
+                                  "Email/Mobile/Emirates ID",
+                                  _nameController,
+                                  Icon(
+                                    Icons.person,
+                                    size: 20,
+                                    color: isDarkMode
+                                        ? Colors.white
+                                        : Colors.black,
+                                  )),
+                              SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  _buildLabelText(
+                                      context,
+                                      "Mobile Number e.g.: 971500000000",
+                                      12,
+                                      false,
+                                      Colors.grey),
+                                ],
+                              ),
+                              SizedBox(height: 4),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 28.0),
+                            child: CustomButtonComponent(text: "Continue",
+                                screenWidth: screenWidth,
+                                onTap: () {
+                                  hideKeyBoard();
+                                  Navigator.pushNamed(context, "/BottomNav");
+                                }),
+                          ),
+                        ],
+                      )),
+                ),
+              ),
+              isLoading
+                  ? Stack(
+                children: [
+                  // Block interaction
+                  ModalBarrier(
+                      dismissible: false, color: Colors.transparent),
+                  // Loader indicator
+                  Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ],
+              )
+                  : SizedBox()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildLabelText(BuildContext context, String text, int size, bool isBold,
+      Color grey) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: size.toDouble(),
+        color: grey,
+        fontWeight: isBold ? FontWeight.w600 : FontWeight.normal,
+      ),
+    );
+  }
+
+  Widget _buildPhoneInput(BuildContext context, String text,
+      TextEditingController nameController, Icon icon) {
+    return Card(
+      elevation: 0,
+      child: Container(
+        height: 60,
+        padding: EdgeInsets.symmetric(horizontal: 4.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(width: 0.2, color: Colors.grey)),
+        child: Row(
+          children: [
+            SizedBox(width: 16),
+            Expanded(
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+                obscureText: false,
+                obscuringCharacter: "*",
+                controller: nameController,
+                onChanged: (value) {
+                  _isValidInput();
+                },
+                onSubmitted: (value) {},
+                keyboardType: TextInputType.visiblePassword,
+                textInputAction: TextInputAction.done,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(
+                    RegExp(r'[a-zA-Z0-9@._]'),
+                  ),
+                  FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                ],
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: text,
+                  //icon: icon,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void Validate(String email) {
+    bool isValid = EmailValidator.validate(email);
+    print(isValid);
+  }
+}
---
+++ bdpass/lib/view/screens/authSection/verification_screen.dart
@@ -0,0 +1,211 @@
+import 'dart:io';
+
+import 'package:BDPass/languageSection/Languages.dart';
+import 'package:BDPass/utils/Helper.dart';
+import 'package:BDPass/view/component/custom_button_component.dart';
+import 'package:cunning_document_scanner/cunning_document_scanner.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../model/apis/api_response.dart';
+import '../../../model/response/countryListResponse.dart';
+import '../../../view_model/main_view_model.dart';
+import '../../component/connectivity_service.dart';
+import '../../component/instruction_step.dart';
+
+class VerificationScreen extends StatefulWidget {
+  @override
+  _VerificationScreenState createState() => _VerificationScreenState();
+}
+
+class _VerificationScreenState extends State<VerificationScreen> {
+  String token = "";
+  late double screenWidth;
+  late double screenHeight;
+  PageController _pageController = PageController();
+  bool isLoading = false;
+  final ConnectivityService _connectivityService = ConnectivityService();
+  static const maxDuration = Duration(seconds: 2);
+  List<CountryData> countryList = [];
+  File? docImg;
+
+  @override
+  void initState() {
+    super.initState();
+    //_fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back)),
+      ),
+      body: SafeArea(
+        child: Stack(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 14),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 8,
+                ),
+                Image(
+                  height: screenHeight * 0.06,
+                  image: AssetImage(isDarkMode
+                      ? "assets/app_logo_dark.png"
+                      : "assets/app_logo.png"),
+                  fit: BoxFit.cover,
+                ),
+                /*Text(
+                  "BD ID",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),*/
+                Row(
+                  children: [
+                    InstructionStep(
+                        icon: Icons.document_scanner_rounded,
+                        title: "Step 1",
+                        isActive: true,
+                        iconColor: Colors.green.shade900),
+                    InstructionStep(
+                        icon: Icons.person_sharp,
+                        title: "Step 2",
+                        isActive: false,
+                        iconColor: Colors.green.shade900),
+                    InstructionStep(
+                        icon: Icons.lock_sharp,
+                        title: "Step 3",
+                        isActive: false,
+                        iconColor: Colors.green.shade900),
+                  ],
+                ),
+                Text(
+                  "Scan Your BD ID",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Use your device's camera to scan your BD ID",
+                  style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
+                ),
+                Image(
+                  //alignment: Alignment.topLeft,
+                  width: screenWidth,
+                  height: screenHeight * 0.5,
+                  image: AssetImage("assets/slide_1.png"),
+                ),
+                Spacer(),
+                CustomButtonComponent(text: "Scan Now",
+                    screenWidth: screenWidth, onTap: () {
+                  //Navigator.pushNamed(context, "/PhoneVerificationScreen");
+                  Navigator.pushNamed(context, "/FaceDetectorView");
+                }),
+                SizedBox(
+                  height: 35,
+                )
+              ],
+            ),
+          ),
+          isLoading
+              ? Stack(
+                  children: [
+                    // Block interaction
+                    ModalBarrier(dismissible: false, color: Colors.transparent),
+                    // Loader indicator
+                    Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ],
+                )
+              : SizedBox()
+        ]),
+      ),
+    );
+  }
+
+  void onPressedFrontImage() async {
+    List<String> pictures;
+    try {
+      pictures = await CunningDocumentScanner.getPictures(noOfPages: 1) ?? [];
+      if (!mounted) return;
+      setState(() {
+        print("Front Image: ${pictures}");
+        docImg = File(pictures.first);
+        print("Front Image: $docImg");
+      });
+    } catch (exception) {
+      // Handle exception here
+    }
+  }
+
+  void _fetchData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    bool isConnected = await _connectivityService.isConnected();
+    if (!isConnected) {
+      setState(() {
+        isLoading = false;
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content:
+                Text('${Languages.of(context)?.labelNoInternetConnection}'),
+            duration: maxDuration,
+          ),
+        );
+      });
+    } else {
+      await Future.delayed(Duration(milliseconds: 2));
+      await Provider.of<MainViewModel>(context, listen: false)
+          .fetchCountryList("api/v1/app/customers/country_list");
+      ApiResponse apiResponse =
+          Provider.of<MainViewModel>(context, listen: false).response;
+      getCountryList(context, apiResponse);
+    }
+  }
+
+  Widget getCountryList(BuildContext context, ApiResponse apiResponse) {
+    CountryListResponse? countryListResponse =
+        apiResponse.data as CountryListResponse?;
+    var message = apiResponse.message.toString();
+    print("message ${message}");
+    setState(() {
+      isLoading = false;
+    });
+    switch (apiResponse.status) {
+      case Status.LOADING:
+        return Center(child: CircularProgressIndicator());
+      case Status.COMPLETED:
+        print("rwrwr ${countryListResponse?.countries?[1].name}");
+
+        countryList = countryListResponse!.countries!;
+        Helper.saveCountryList(countryList);
+        //selectedItem = "${countryListResponse?.countries?[0].flagImageUrl}";
+
+        print("countriess ${countryList}");
+
+        //_showPicker(context: context);
+
+        return Container(); // Return an empty container as you'll navigate away
+      case Status.ERROR:
+        print("countriess ${countryList}");
+        return Center(
+          child: Text('Please try again later!!!'),
+        );
+      case Status.INITIAL:
+      default:
+        return Center(
+          child: Text(''),
+        );
+    }
+  }
+}
